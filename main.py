5918a11be9d49219eaabba9d12724cab0a0ca187
loyilok515 <enoch.lo@mail.utoronto.ca>
2026-02-23 11:19:34 -0500

    Combined two versions of main.py

--- main.py
+++ main.py
@@ -22,13 +22,14 @@ learning_rate = 0.001  # Base learning rate
 epochs = 45  # Number of training epochs
 lr_step = 15  # Learning rate step
 detach_epoch = 10  # Number of epochs to detach the computation graph
-_lambda = 0.5  # Convergence rate: lambda
+_lambda_fixed = 0.5  # Convergence rate: lambda
 w_ub = 10  # Upper bound of the eigenvalue of the dual metric
 w_lb = 0.1  # Lower bound of the eigenvalue of the dual metric
+alpha_lb = 0.5  # Lower bound of the alpha parameter in RCCM
 
 # Configuration variables
-task = 'quad'  # Name of the model
-log = 'log_quad_constrained'  # Path to a directory for storing the training log
+task = 'quad_delay'  # Name of the model
+log = 'log_quad_delay_lambda_RCCM'  # Path to a directory for storing the training log
 use_cuda = True  # Set to False to disable CUDA
 
 np.random.seed(1024)
@@ -41,7 +42,7 @@ os.system('cp -r models/ '+log)
 os.system('cp -r configs/ '+log)
 os.system('cp -r systems/ '+log)
 
-epsilon = _lambda * 0.1  # Ensure definiteness of LMI
+epsilon = _lambda_fixed * 0.1  # Ensure definiteness of LMI
 
 # Get training bounds from config file
 config = importlib.import_module('config_'+task)
@@ -51,23 +52,30 @@ U_MIN = config.UREF_MIN
 U_MAX = config.UREF_MAX
 XE_MIN = config.XE_MIN
 XE_MAX = config.XE_MAX
+w_MIN = config.w_MIN
+w_MAX = config.w_MAX
 
 # Get system functions from system file
 system = importlib.import_module('system_'+task)
 S_func = system.S_func
 f_func = system.f_func
 B_func = system.B_func
+B_w_func = system.B_w_func
+C_func = system.C_func
+D_func = system.D_func
 E_func = system.E_func
 Ebot_func = system.Ebot_func
 num_dim_x = system.num_dim_x
 num_dim_control = system.num_dim_control
 num_dim_manifold = system.num_dim_manifold
+num_dim_noise = system.num_dim_noise
+num_dim_z = system.num_dim_z
 
 # Get neural network configurations from model file
 model = importlib.import_module('model_'+task)
 get_model = model.get_model
 
-model_W, model_Wbot, model_u_w1, model_u_w2, W_func, u_func = get_model(num_dim_x, num_dim_manifold, num_dim_control, w_lb=w_lb, use_cuda=use_cuda)
+model_W, model_Wbot, model_u_w1, model_u_w2, param_alpha, param_miu, param_lambda, W_func, u_func = get_model(num_dim_x, num_dim_manifold, num_dim_control, w_lb=w_lb, use_cuda=use_cuda)
 
 # Generate training samples
 def sample_xef():
@@ -106,11 +114,15 @@ def sample_uref():
     # Random sample ustar based on sampling bounds
     return (U_MAX-U_MIN) * np.random.rand(num_dim_control, 1) + U_MIN
 
+def sample_w():
+    return (w_MAX-w_MIN) * np.random.rand(num_dim_noise, 1) + w_MIN
+
 def sample_full():
     xref = sample_xef()
     uref = sample_uref()
     x = sample_x(xref)
-    return (x, xref, uref)
+    w = sample_w()
+    return (x, xref, uref, w)
 
 X_tr = [sample_full() for _ in range(num_train)]
 X_te = [sample_full() for _ in range(num_test)]
@@ -178,7 +190,7 @@ def loss_pos_matrix_eigen_values(A):
     negative_eigv = eigv[negative_index]
     return negative_eigv.norm()
 
-def forward(x, xref, uref, _lambda, verbose=False, acc=False, detach=False):
+def forward(x, xref, uref, w, verbose=False, acc=False, detach=False):
     # x: bs x n x 1
     bs = x.shape[0]
     W = W_func(x) # W: bs x num_dim_manifold x num_dim_manifold
@@ -188,18 +200,31 @@ def forward(x, xref, uref, _lambda, verbose=False, acc=False, detach=False):
     P_s_T = P_s.transpose(1,2)
     f = f_func(x)
     B = B_func(x)
+    B_w = B_w_func(x)
+    C = C_func(x)
+    D = D_func(x)
     E = E_func(x)
     _Ebot = Ebot_func(x)
     DfDx = Jacobian(f, x)
     DBDx = torch.zeros(bs, num_dim_x, num_dim_x, num_dim_control).type(x.type())
     for i in range(num_dim_control):
         DBDx[:,:,:,i] = Jacobian(B[:,:,i].unsqueeze(-1), x)
+    DBwDx = torch.zeros(bs, num_dim_x, num_dim_x, num_dim_noise).type(x.type())
+    for i in range(num_dim_noise):
+        DBwDx[:,:,:,i] = Jacobian(B_w[:,:,i].unsqueeze(-1), x)
 
     u = u_func(x, xref, uref) # u: bs x m x 1
     K = Jacobian(u, x)
 
-    A = DfDx + sum([u[:, i, 0].unsqueeze(-1).unsqueeze(-1) * DBDx[:, :, :, i] for i in range(num_dim_control)])   
-    dot_x = f + B.matmul(u)
+    # compute RCCM parameters (force positive constants)
+    _alpha = F.softplus(param_alpha)
+    _miu = F.softplus(param_miu)
+    _lambda_nn = F.softplus(param_lambda)
+    _lambda = _lambda_nn #  _lambda = _lambda_fixed
+
+    A = ( DfDx + sum([u[:, i, 0].unsqueeze(-1).unsqueeze(-1) * DBDx[:, :, :, i] for i in range(num_dim_control)])
+             + sum([w[:, i, 0].unsqueeze(-1).unsqueeze(-1) * DBwDx[:, :, :, i] for i in range(num_dim_noise)])  )
+    dot_x = f + B.matmul(u) + B_w.matmul(w)
     dot_M = weighted_gradients(M, dot_x, x, detach=detach) # DMDt
     dot_W = weighted_gradients(W, dot_x, x, detach=detach) # DWDt
     dot_P_s_T = weighted_gradients(P_s_T, dot_x, x, detach=detach)  # DPsTdt
@@ -212,8 +237,30 @@ def forward(x, xref, uref, _lambda, verbose=False, acc=False, detach=False):
     # Contraction condition (detach for first few epochs to let C1 and C2 find a CCM first)
     if detach:
         Contraction = dot_M + ((dot_P_s_T + P_s_T.matmul(A) + E.matmul(K)).matmul(S)).transpose(1,2).matmul(M.detach()) + M.detach().matmul((dot_P_s_T + P_s_T.matmul(A) + E.matmul(K)).matmul(S)) + 2 * _lambda * M.detach()
+        Contraction_dual = -dot_W + W.detach().matmul(((dot_P_s_T + P_s_T.matmul(A) + E.matmul(K)).matmul(S)).transpose(1,2)) + ((dot_P_s_T + P_s_T.matmul(A) + E.matmul(K)).matmul(S)).matmul(W.detach()) + 2 * _lambda * W.detach()
     else:
         Contraction = dot_M + ((dot_P_s_T + P_s_T.matmul(A) + E.matmul(K)).matmul(S)).transpose(1,2).matmul(M) + M.matmul((dot_P_s_T + P_s_T.matmul(A) + E.matmul(K)).matmul(S)) + 2 * _lambda * M
+        Contraction_dual = -dot_W + W.matmul(((dot_P_s_T + P_s_T.matmul(A) + E.matmul(K)).matmul(S)).transpose(1,2)) + ((dot_P_s_T + P_s_T.matmul(A) + E.matmul(K)).matmul(S)).matmul(W) + 2 * _lambda * W
+
+    # RCCM_1 (Condition (15))
+    RCCM_1_11 = Contraction_dual
+    RCCM_1_12 = P_s_T.matmul(B_w)
+    RCCM_1_21 = RCCM_1_12.transpose(1,2)
+    RCCM_1_22 = - _miu * torch.eye(num_dim_noise).repeat(bs, 1, 1).type(x.type())
+    RCCM_1 = torch.cat([
+        torch.cat([RCCM_1_11, RCCM_1_12], dim=2),
+        torch.cat([RCCM_1_21, RCCM_1_22], dim=2)
+    ], dim=1)
+
+    # RCCM_2 (Condition (16))
+    RCCM_2_11 = 2*_lambda*M - (S.transpose(1,2)).matmul((C + D.matmul(K)).transpose(1,2)).matmul(C + D.matmul(K)).matmul(S) / _alpha
+    RCCM_2_12 = torch.zeros(bs, num_dim_manifold, num_dim_noise).type(x.type())
+    RCCM_2_21 = RCCM_2_12.transpose(1,2)
+    RCCM_2_22 = (_alpha - _miu) * torch.eye(num_dim_noise).repeat(bs, 1, 1).type(x.type())
+    RCCM_2 = torch.cat([
+        torch.cat([RCCM_2_11, RCCM_2_12], dim=2),
+        torch.cat([RCCM_2_21, RCCM_2_22], dim=2)
+    ], dim=1)
 
     # C1
     C1_inner = - weighted_gradients(W, f, x) + S_f.matmul(W) + W.matmul(S_f.transpose(1,2)) + 2 * _lambda * W
@@ -228,26 +275,35 @@ def forward(x, xref, uref, _lambda, verbose=False, acc=False, detach=False):
         C2_inners.append(C2_inner)
         C2s.append(C2)
 
-    loss = 0
-    loss += loss_pos_matrix_random_sampling(-Contraction - epsilon * torch.eye(Contraction.shape[-1]).unsqueeze(0).type(x.type()))
-    loss += loss_pos_matrix_random_sampling(-C1_LHS_1 - epsilon * torch.eye(C1_LHS_1.shape[-1]).unsqueeze(0).type(x.type()))
-    loss += loss_pos_matrix_random_sampling(w_ub * torch.eye(W.shape[-1]).unsqueeze(0).type(x.type()) - W)
-    loss += 4. * sum([1.*(C2**2).reshape(bs,-1).sum(dim=1).mean() for C2 in C2s])
+    loss_CCM = loss_pos_matrix_random_sampling(-Contraction - epsilon * torch.eye(Contraction.shape[-1]).unsqueeze(0).type(x.type()))
+    loss_w_ub = loss_pos_matrix_random_sampling(w_ub * torch.eye(W.shape[-1]).unsqueeze(0).type(x.type()) - W)
+    loss_C1 = loss_pos_matrix_random_sampling(-C1_LHS_1 - epsilon * torch.eye(C1_LHS_1.shape[-1]).unsqueeze(0).type(x.type()))
+    loss_C2 = 4. * sum([1.*(C2**2).reshape(bs,-1).sum(dim=1).mean() for C2 in C2s])
+    loss_RCCM1 = loss_pos_matrix_random_sampling(-RCCM_1 - epsilon * torch.eye(RCCM_1.shape[-1]).unsqueeze(0).type(x.type()))
+    loss_RCCM2 = loss_pos_matrix_eigen_values(RCCM_2 - epsilon * torch.eye(RCCM_2.shape[-1]).unsqueeze(0).type(x.type()))
+    loss_alpha = torch.relu(_alpha - alpha_lb)
 
-    # Add a penalty for control inputs larger than 3
-    control_penalty = torch.relu((u-uref).abs() - 10).sum(dim=1).mean()
-    # loss += 1. * control_penalty
+    loss = loss_CCM + loss_w_ub + loss_C1 + loss_C2 + loss_RCCM1 + loss_RCCM2 + loss_alpha
 
     if verbose:
-        print(torch.symeig(Contraction)[0].min(dim=1)[0].mean(), torch.symeig(Contraction)[0].max(dim=1)[0].mean(), torch.symeig(Contraction)[0].mean())
+        print("loss_CCM: %.6f, loss_w_ub: %.6f, loss_C1: %.6f, loss_C2: %.6f, loss_RCCM1: %.6f, loss_RCCM2: %.6f, loss_alpha: %.6f"%(loss_CCM.item(), loss_w_ub.item(), loss_C1.item(), loss_C2.item(), loss_RCCM1.item(), loss_RCCM2.item(), loss_alpha.item()))
+        print(torch.linalg.eigvalsh(Contraction).min(dim=1)[0].mean().item(), torch.linalg.eigvalsh(Contraction).max(dim=1)[0].mean().item(), torch.linalg.eigvalsh(Contraction).mean().item())
+        print(torch.linalg.eigvalsh(RCCM_1).min(dim=1)[0].mean().item(), torch.linalg.eigvalsh(RCCM_1).max(dim=1)[0].mean().item(), torch.linalg.eigvalsh(RCCM_1).mean().item())
+        print(torch.linalg.eigvalsh(RCCM_2).min(dim=1)[0].mean().item(), torch.linalg.eigvalsh(RCCM_2).max(dim=1)[0].mean().item(), torch.linalg.eigvalsh(RCCM_2).mean().item())
     if acc:
-        return loss, ((torch.linalg.eigvalsh(Contraction, UPLO='L') >= 0).sum(dim=1) == 0).cpu().detach().numpy(), ((torch.linalg.eigvalsh(C1_LHS_1, UPLO='L') >= 0).sum(dim=1) == 0).cpu().detach().numpy(), sum([1.*(C2**2).reshape(bs,-1).sum(dim=1).mean() for C2 in C2s]).item(), control_penalty.item()
+        return (loss, 
+               ((torch.linalg.eigvalsh(Contraction, UPLO='L') >= 0).sum(dim=1) == 0).cpu().detach().numpy(), 
+               ((torch.linalg.eigvalsh(C1_LHS_1, UPLO='L') >= 0).sum(dim=1) == 0).cpu().detach().numpy(), 
+               sum([1.*(C2**2).reshape(bs,-1).sum(dim=1).mean() for C2 in C2s]).item(), 
+               ((torch.linalg.eigvalsh(RCCM_1, UPLO='L') >= 0).sum(dim=1) == 0).cpu().detach().numpy(),
+               ((torch.linalg.eigvalsh(RCCM_2, UPLO='L') <= 0).sum(dim=1) == 0).cpu().detach().numpy()
+               )
     else:
-        return loss, None, None, None, None
+        return loss, None, None, None, None, None
 
-optimizer = torch.optim.Adam(list(model_W.parameters()) + list(model_Wbot.parameters()) + list(model_u_w1.parameters()) + list(model_u_w2.parameters()), lr=learning_rate)
+optimizer = torch.optim.Adam([param_alpha, param_miu, param_lambda] + list(model_W.parameters()) + list(model_Wbot.parameters()) + list(model_u_w1.parameters()) + list(model_u_w2.parameters()), lr=learning_rate)
 
-def trainval(X, bs=bs, train=True, _lambda=_lambda, acc=False, detach=False): # trainval a set of x
+def trainval(X, bs=bs, train=True, acc=False, detach=False): # trainval a set of x
     # torch.autograd.set_detect_anomaly(True)
 
     if train:
@@ -259,7 +315,8 @@ def trainval(X, bs=bs, train=True, _lambda=_lambda, acc=False, detach=False): # 
     total_p1 = 0
     total_p2 = 0
     total_l3 = 0
-    total_c4 = 0
+    total_r4 = 0
+    total_r5 = 0
 
     if train:
         _iter = tqdm(range(len(X) // bs))
@@ -267,38 +324,36 @@ def trainval(X, bs=bs, train=True, _lambda=_lambda, acc=False, detach=False): # 
         _iter = range(len(X) // bs)
     for b in _iter:
         start = time.time()
-        x = []; xref = []; uref = []; 
+        x = []; xref = []; uref = []; w = []
         for id in indices[b*bs:(b+1)*bs]:
             if use_cuda:
                 x.append(torch.from_numpy(X[id][0]).float().cuda())
                 xref.append(torch.from_numpy(X[id][1]).float().cuda())
                 uref.append(torch.from_numpy(X[id][2]).float().cuda())
+                w.append(torch.from_numpy(X[id][3]).float().cuda())
             else:
                 x.append(torch.from_numpy(X[id][0]).float())
                 xref.append(torch.from_numpy(X[id][1]).float())
                 uref.append(torch.from_numpy(X[id][2]).float())
-
-        x, xref, uref = (torch.stack(d).detach() for d in (x, xref, uref))
+                w.append(torch.from_numpy(X[id][3]).float())
+        x, xref, uref, w = (torch.stack(d).detach() for d in (x, xref, uref, w))
         x = x.requires_grad_()
 
-        start = time.time()
+        loss, p1, p2, l3, r4, r5 = forward(x, xref, uref, w, verbose=True if not train else False, acc=acc, detach=detach)
 
-        loss, p1, p2, l3, c4 = forward(x, xref, uref, _lambda=_lambda, verbose=False if not train else False, acc=acc, detach=detach)
-
-        start = time.time()
         if train:
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
-            # print('backward(): %.3f s'%(time.time() - start))
 
         total_loss += loss.item() * x.shape[0]
         if acc:
             total_p1 += p1.sum()
             total_p2 += p2.sum()
             total_l3 += l3 * x.shape[0]
-            total_c4 += c4 * x.shape[0]
-    return total_loss / len(X), total_p1 / len(X), total_p2 / len(X), total_l3/ len(X), total_c4 / len(X)
+            total_r4 += r4.sum()
+            total_r5 += r5.sum()
+    return total_loss / len(X), total_p1 / len(X), total_p2 / len(X), total_l3/ len(X), total_r4 / len(X), total_r5 / len(X)
 
 
 best_acc = 0
@@ -312,21 +367,28 @@ def adjust_learning_rate(optimizer, epoch):
 
 for epoch in range(0, epochs):
     adjust_learning_rate(optimizer, epoch)
-    loss, _, _, _, _ = trainval(X_tr, train=True, _lambda=_lambda, acc=False, detach=True if epoch < detach_epoch else False)
+    loss, _, _, _, _, _ = trainval(X_tr, train=True, acc=False, detach=True if epoch < detach_epoch else False)
     print("Training loss: ", loss)
-    loss, p1, p2, l3, c4 = trainval(X_te, train=False, _lambda=0., acc=True, detach=False)
-    print("Epoch %d: Testing loss/p1/p2/l3/c4: "%epoch, loss, p1, p2, l3, c4)  # Ideal: 0 (total loss), 1 (probability of contraction condition being satisfied), 1 (probability of C1 being satisfied), 0 (mean of C2), 0 (mean of control penalty)
+    loss, p1, p2, l3, r4, r5 = trainval(X_te, train=False, acc=True, detach=False)
+    print("Epoch %d: Testing loss/p1/p2/l3/r4/r5: "%epoch, loss, p1, p2, l3, r4, r5)  # Ideal: 0 / 1 / 1 / 0 / 1 / 1
+    alpha_curr = F.softplus(param_alpha)
+    miu_curr = F.softplus(param_miu)
+    lambda_curr = F.softplus(param_lambda)
+    print("alpha: %.3f, miu: %.3f, lambda: %.3f"%(alpha_curr.item(), miu_curr.item(), lambda_curr.item()))
 
     if p1+p2 >= best_acc:
         best_acc = p1 + p2
         filename = log+'/model_best.pth.tar'
         filename_controller = log+'/controller_best.pth.tar'
         torch.save({
-            'precs': (loss, p1, p2, l3, c4),
+            'precs': (loss, p1, p2, l3, r4, r5),
             'model_W': model_W.state_dict(),
             'model_Wbot': model_Wbot.state_dict(),
             'model_u_w1': model_u_w1.state_dict(),
             'model_u_w2': model_u_w2.state_dict(),
+            'alpha': alpha_curr.item(),
+            'miu': miu_curr.item(),
+            'lambda': lambda_curr.item(),
             'optimizer': optimizer.state_dict(),
             'epoch': epoch
         }, filename)
